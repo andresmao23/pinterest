bda4f152bb9f270cf80ade1eec86ac4b0046ea46
Mao <andresmao23@hotmail.com>
2016-01-25 09:44:41 -0500

    Installing and using devise crating user table, relationchip between user and pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	validates :name, presence:true
 	validates :photo, presence:true
 	validates :description, presence:true
+
+	belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :pins
   get 'welcome/index'
   root 'welcome#index'
+  get 'mypins' => 'pins#mypins'
+  get 'pinsof/:user_id' => 'pins#pinsof', :as => 'pinsof'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
